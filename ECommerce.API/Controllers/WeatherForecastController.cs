b84ab23431ded1de52658b61e74c7cc3409d6c69
Kamal Singh Bora <thekamalbora@yahoo.com>
2026-06-25 23:09:41 +0530

    weatherapi

--- ECommerce.API/Controllers/WeatherForecastController.cs
+++ ECommerce.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ECommerce.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy","Fizzy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
