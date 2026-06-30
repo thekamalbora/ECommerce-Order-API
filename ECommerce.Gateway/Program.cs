97144d53f1247c361ef7cce41530c105cba39400
Kamal Singh Bora <thekamalbora@yahoo.com>
2026-07-01 00:13:30 +0530

    feat(gateway): setup YARP API Gateway with microservices routing configuration

---
+++ ECommerce.Gateway/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ---------------- YARP ----------------
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// ---------------- JWT ----------------
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+// IMPORTANT: policy define karna safe side ke liye
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Authenticated", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+    });
+});
+
+var app = builder.Build();
+
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// YARP
+app.MapReverseProxy();
+
+app.Run();
